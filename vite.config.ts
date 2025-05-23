42bec125e7441e7bc39e344f28beecf87f31acf2
𝑵𝒊𝒌𝒊𝒕𝒂 𝑩𝒖𝒚𝒂𝒏𝒐𝒗🧸 <119776926+N1koir@users.noreply.github.com>
2025-05-24 04:28:43 +0900

    Updated index.html

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   server: {
@@ -16,6 +15,12 @@ export default defineConfig({
         target: 'http://localhost:5245',
         changeOrigin: true,
       },
+    },
+  },
+  css: {
+    modules: {
+      localsConvention: 'camelCase',
+      generateScopedName: '[name]__[local]__[hash:base64:5]',
     },
   },
 });
